b504e4b9b9ff94dfad5b7e0004e6113890bc6f2e
tr1b1iwnl <tr1b1iwnl@gmail.com>
2022-12-14 12:10:43 +0800

    Complete SimpleCalc

--- src/SimpleCalc/SimpleCalcGUI.java
+++ src/SimpleCalc/SimpleCalcGUI.java
@@ -1,8 +1,11 @@
 package SimpleCalc;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class SimpleCalcGUI extends JFrame {
+    private JFrame frame;
     private JPanel panel1;
     private JTextField tfNumber1;
     private JComboBox cbOperations;
@@ -10,7 +13,41 @@ public class SimpleCalcGUI extends JFrame {
     private JTextField tfNumber2;
     private JTextField lblResult;
 
-    public static void main(String[] args) {
+    public SimpleCalcGUI(){
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(600, 300);
+        frame.setTitle("Simple Calculator");
+        frame.setVisible(true);
+        frame.add(panel1);
+        tfNumber1.setHorizontalAlignment(SwingConstants.CENTER);
+        tfNumber2.setHorizontalAlignment(SwingConstants.CENTER);
+        lblResult.setHorizontalAlignment(SwingConstants.CENTER);
 
+        btnCompute.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int num1 = Integer.parseInt(tfNumber1.getText());
+                int num2 = Integer.parseInt(tfNumber2.getText());
+                char op =  cbOperations.getSelectedItem().toString().charAt(0);
+                int result;
+                if(op == '+') {
+                    result = num1 + num2;
+                } else if (op == '-') {
+                    result = num1 - num2;
+                }
+                else if(op == '*') {
+                    result = num1 * num2;
+                }
+                else {
+                    result = num1 / num2;
+                }
+                lblResult.setText(Integer.toString(result));
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SimpleCalcGUI scGUI = new SimpleCalcGUI();
     }
 }
